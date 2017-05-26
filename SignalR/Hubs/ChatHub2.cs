59f3cfa3c94c6c223c3bc37bbf0b2c4d993a7381
Amir <a_5060@live.com>
2017-05-26 07:39:30 +0300

    IoC

--- SignalR/Hubs/ChatHub2.cs
+++ SignalR/Hubs/ChatHub2.cs
@@ -16,6 +16,7 @@ namespace SignalR.Hubs
 
         public void Send(string name, string message)
         {
+
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
             CloudTable table = MessageTable();
